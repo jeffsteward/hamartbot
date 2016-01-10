fa4997c53957b4236374a45183030eedaf275b37
Jeff Steward <steward.jeff@gmail.com>
2016-01-10 16:38:09 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# References
+
+There are so many good examples of Twitter bots. Here are a few that I used. 
+
+https://github.com/dariusk/examplebot  
+https://github.com/dariusk/museumbot  
+http://readwrite.com/2014/06/20/random-non-sequitur-twitter-bot-instructions
---
+++ index.js
@@ -0,0 +1,33 @@
+var fs = require("fs"),
+	syncrequest = require("sync-request"),
+	request = require("request"),
+	Twit = require("twit"),
+	config = require("./config.js");
+
+var apiURL = 'http://api.harvardartmuseums.org';
+var apiKey = config.ham.apikey;
+
+
+//Fetch some stuff from the HAM API
+var res = syncrequest('GET', apiURL + '/object?apikey=' + apiKey + '&sort=random&hasimage=1&size=1&fields=title,primaryimageurl,url');
+var o = JSON.parse(res.getBody())
+
+//Fetch the art image
+var res = syncrequest('GET', o.records[0].primaryimageurl);
+var i = res.getBody();
+fs.writeFileSync('artimage.jpg', i, 'binary');
+
+//Post some stuff to Twitter @hamartbot
+var T = new Twit(config.twitter);
+
+//First upload the image for the tweet
+var b64content = fs.readFileSync('artimage.jpg', {encoding: 'base64'});
+T.post('media/upload', { media_data: b64content}, function(err, data, response) {
+	var mediaID = data.media_id_string;
+	
+	//Second tweet the new status with the image	
+	var statusMessage = o.records[0].title + ' ' + o.records[0].url;
+	T.post('statuses/update', { status: statusMessage, media_ids: [mediaID] }, function(err, data, response) {
+	  console.log(data)
+	});
+});
